573ed1ab4be36a71d4f2f954685dce805dba85e2
Nathaniel Backus <nathanielbackus@me.com>
2024-07-19 13:37:41 -0700

    removing dbmanager for time being

--- app/src/main/java/com/example/c195mobileapp/DatabaseManager.java
+++ app/src/main/java/com/example/c195mobileapp/DatabaseManager.java
@@ -1,26 +1,26 @@
-package com.example.c195mobileapp;
-
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-
-public class DatabaseManager {
-    private DatabaseHelper dbHelper;
-    private Context context;
-    private SQLiteDatabase database;
-    public DatabaseManager(Context ctx){
-        context = ctx;
-    }
-    public DatabaseManager open() throws {
-        dbHelper = new DatabaseHelper(context);
-        database = dbHelper.getWritableDatabase();
-        return this;
-    }
-    public void close() {
-        dbHelper.close();
-    }
-    public void insert (String username, String password){
-        ContentValues contentValues = new ContentValues();
-        ContentValues.put
-    }
-}
+//package com.example.c195mobileapp;
+//
+//import android.content.ContentValues;
+//import android.content.Context;
+//import android.database.sqlite.SQLiteDatabase;
+//
+//public class DatabaseManager {
+//    private DatabaseHelper dbHelper;
+//    private Context context;
+//    private SQLiteDatabase database;
+//    public DatabaseManager(Context ctx){
+//        context = ctx;
+//    }
+//    public DatabaseManager open() throws {
+//        dbHelper = new DatabaseHelper(context);
+//        database = dbHelper.getWritableDatabase();
+//        return this;
+//    }
+//    public void close() {
+//        dbHelper.close();
+//    }
+//    public void insert (String username, String password){
+//        ContentValues contentValues = new ContentValues();
+//        ContentValues.put
+//    }
+//}
